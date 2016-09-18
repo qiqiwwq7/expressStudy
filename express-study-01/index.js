9f9a343bb1cb511220739d567ebfef4b22c82e94
songruigang <relign@songruiangdeMBP.lan>
2016-09-18 22:35:16 +0800

    commit

--- express-study-01/index.js
+++ express-study-01/index.js
@@ -34,26 +34,6 @@
   /// dynamically include routers
   app.use(enrouten({directory: 'routers'}));
 
-
-
-  app.post('/process',function (req, res) {
-    console.log('Form (form querystring): ' + req.query.form);
-    console.log('CSRF token (from hidden form field): ' + req.body._crsf);
-    console.log('Name (from visible form field): ' + req.body.name);
-    console.log('Email (from visibel form field): ' + req.body.email);
-    res.redirect(303,'/thank-you');
-  })
-
-  app.post('/process/ajax',function (req, res) {
-    if(req.xhr || req.accepts('json,html')==='json'){
-      //如果发生错误，应该发送{error : 'error description'}
-      res.send({success: true});
-    }else{
-      //如果发生错误，应该重定向到错误页面
-      res.redirect(303, '/thank-you');
-    }
-  })
-
   app.post('/process-contact',function (req,res) {
     console.log('Received contact from ' + req.body.name + ' <' + req.body.email + '>');
     try {
---
+++ express-study-01/routers/process/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = function (router) {
+
+  router.post('/',function (req, res) {
+    console.log('Form (form querystring): ' + req.query.form);
+    console.log('CSRF token (from hidden form field): ' + req.body._crsf);
+    console.log('Name (from visible form field): ' + req.body.name);
+    console.log('Email (from visibel form field): ' + req.body.email);
+    res.redirect(303,'/thank-you');
+  })
+
+  router.post('/ajax',function (req, res) {
+    if(req.xhr || req.accepts('json,html')==='json'){
+      //如果发生错误，应该发送{error : 'error description'}
+      res.send({success: true});
+    }else{
+      //如果发生错误，应该重定向到错误页面
+      res.redirect(303, '/thank-you');
+    }
+  })
+}
